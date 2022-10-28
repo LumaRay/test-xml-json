a891399b3f11354d52524a7743c2dd4039219d69
Yury Laykov <sowulo@inbox.ru>
2022-10-28 19:05:02 +0300

    Delete test-rust-pugixml directory

--- test-rust-pugixml/rust-pugixml/src/lib.rs
+++
@@ -1,8 +0,0 @@
-extern crate libc;
-
-pub use self::misc::version;
-
-mod misc;
-mod wrapper;
-mod helpers;
-pub mod tree;
--- test-rust-pugixml/rust-pugixml/src/misc.rs
+++
@@ -1,6 +0,0 @@
-pub fn version() -> i32 {
-    use wrapper::pugi_version;
-    unsafe {
-        return pugi_version();
-    }
-}
